a3025dea16c6d61bd1de88696a9bf0a315b2987c
Skorban <skorban@outlook.com>
2021-03-28 20:26:31 +0200

    Initial Commit

---
+++ Pogoda/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pogoda
+//
+//  Created by Paweł Ładna on 28/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pogoda/PogodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PogodaApp.swift
+//  Pogoda
+//
+//  Created by Paweł Ładna on 28/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PogodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
